c1cc50342aa74542d32661fd50bdcd6b4b47b916
athospugliesedev <118693085+athospugliesedev@users.noreply.github.com>
2024-03-12 10:44:08 -0300

    Update query.sql

--- queries/2/query.sql
+++ queries/2/query.sql
@@ -1,9 +1,8 @@
 Qual o produto que tem mais saída?
 
-SELECT prod_id,
-SUM(qtd) AS total_saida
-FROM silver
+SELECT prod_id, nomeproduto, SUM(qtd) AS total_saidas
+FROM estoque.produto_estoque_gold
 WHERE tipo_de_transacao = 0
-GROUP BY prod_id
-ORDER BY total_saida
-DESC LIMIT 1;
+GROUP BY prod_id, nomeproduto
+ORDER BY total_saidas DESC
+LIMIT 5;
