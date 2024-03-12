3fe759d65f71530d763db7d6063cdfa0fcb2d3e7
athospugliesedev <118693085+athospugliesedev@users.noreply.github.com>
2024-03-12 07:07:53 -0300

    Synchronizing job estoque-transform-bronze-silver from AWS Glue at 2024/03/12 10:07:53

--- scripts/estoque-transform-bronze-silver.py
+++ scripts/estoque-transform-bronze-silver.py
@@ -15,26 +15,28 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-amazonS3 = spark.read.options(delimiter=";", header=True).json("s3://athosbucketimage/bronze/")
+# carregando dados da bronze
+df_bronze = spark.read.options(delimiter=";", header=True).json("s3://athosbucketimage/bronze/")
 
 # transformando tipos dos tipos de dados
-df_normalized = amazonS3.withColumn("prod_id", F.col("prod_id").cast(StringType())) \
-                        .withColumn("id_transacao", F.col("id_transacao").cast(StringType())) \
+df_normalized = df_bronze.withColumn("prod_id", F.col("prod_id").cast(IntegerType())) \
+                        .withColumn("id_transacao", F.col("id_transacao").cast(IntegerType())) \
                         .withColumn("qtd", F.col("qtd").cast(IntegerType())) \
-                        .withColumn("tipo_transacao", F.col("tipo_de_transacao").cast(IntegerType())) \
+                        .withColumn("tipo_de_transacao", F.col("tipo_de_transacao").cast(IntegerType())) \
                         .withColumn("saldo", F.col("saldo").cast(IntegerType())) \
                         .withColumn("data_hora", F.col("timestamp").cast(TimestampType())) \
                         .withColumn("nf_entrada", F.col("nf_entrada").cast(StringType()))
 
-                        
-df_drop = df_formatado.drop(F.col("truncado"))
+# dropando a coluan truncado                        
+df_drop = df_normalized.drop(F.col("truncado"))
 
-df_normalized = df_drop.select("prod_id","id_transacao", "qtd", "tipo_transacao", "saldo", "data_hora", "nf_entrada")
+# Selecionando colunas
+df_result = df_drop.select("prod_id","id_transacao", "qtd", "tipo_de_transacao", "saldo", "data_hora", "nf_entrada")
 
-df_no_duplicates = df_drop.dropDuplicates(["prod_id","id_transacao", "qtd", "tipo_transacao", "saldo", "data_hora", "nf_entrada"])
-df_no_duplicates.show()
+# retirando duplicados
+df_no_duplicates = df_result.dropDuplicates(["prod_id","id_transacao", "qtd", "tipo_de_transacao", "saldo", "data_hora", "nf_entrada"])
 
 database = "estoque"
 tabela = "silver"
 output_path = "s3://athosbucketimage/silver/"
-df_drop.write.insertInto(f"{database}.{tabela}",overwrite=True)
+df_no_duplicates.write.insertInto(f"{database}.{tabela}",overwrite=True)
