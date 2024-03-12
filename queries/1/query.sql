47af488271b0c51e69d7e3633e1d4d277ef70541
athospugliesedev <118693085+athospugliesedev@users.noreply.github.com>
2024-03-12 10:43:42 -0300

    Update query.sql

--- queries/1/query.sql
+++ queries/1/query.sql
@@ -1,6 +1,6 @@
 Qual é o saldo de estoque atual por produto?
 
-SELECT prod_id,
-SUM(saldo) AS saldo_por_produto
-FROM silver
-GROUP BY prod_id;
+SELECT prod_id, nome_produto, SUM(saldo) AS saldo_por_produto
+FROM estoque.produto_estoque_gold
+GROUP BY prod_id, nome_produto
+LIMIT 10;
