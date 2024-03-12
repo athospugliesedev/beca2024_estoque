6c358f6c367339ac102d551f6b70b2c36fc2bb91
athospugliesedev <118693085+athospugliesedev@users.noreply.github.com>
2024-03-12 07:00:33 -0300

    Synchronizing job estoque-silver-gold from AWS Glue at 2024/03/12 10:00:33

--- scripts/estoque-silver-gold.py
+++ scripts/estoque-silver-gold.py
@@ -17,19 +17,13 @@ job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
 # carregando dataframe silver
-amazonS3 = spark.read.options(delimiter=";", header=True).parquet("s3://athosbucketimage/silver/")
-
-# visualizando dataframe estoque
-amazonS3.show()
+df_silver = spark.read.options(delimiter=";", header=True).parquet("s3://athosbucketimage/silver/")
 
 # carregando dataframe produtos
-produto_dataset = spark.read.option("delimiter", "|").option("header", "true").parquet("s3://athosbucketimage/produto_silver/")
+df_produto = spark.read.option("delimiter", "|").option("header", "true").parquet("s3://athosbucketimage/produto_silver/")
 
-# visualizando dataframe produtos
-produto_dataset.show()
-
-# # transformando tipos dos tipos de dados de produto_dataset
-df_produto_normalized = produto_dataset.withColumn("id_produto", F.col("id_produto").cast(IntegerType())) \
+# transformando tipos dos tipos de dados de produto_dataset
+df_produto_normalized = df_produto.withColumn("id_produto", F.col("id_produto").cast(IntegerType())) \
                         .withColumn("nome_produto", F.col("nome_produto").cast(StringType())) \
                         .withColumn("saldo_produto", F.col("saldo_produto").cast(IntegerType())) \
                         .withColumn("custo_produto", F.col("custo_produto").cast(DecimalType())) \
@@ -38,20 +32,11 @@ df_produto_normalized = produto_dataset.withColumn("id_produto", F.col("id_produ
                         .withColumn("marca_produto", F.col("marca_produto").cast(StringType())) \
                         .withColumn("frete_produto", F.col("frete_produto").cast(DecimalType()))
 
-# visualizando dataframe produtos normalizado
-df_produto_normalized.show()
-
-# # renomeando colunas dataframe produtos
-produto_dataset = df_produto_normalized.withColumnRenamed("id_produto", "prod_id") \
-#                                         .withColumnRenamed("Saldo", "saldo_produto") \
-#                                         .withColumnRenamed("NomeProduto", "nome_produto") \
-#                                         .withColumnRenamed("TipoProduto", "tipo_produto")
+# # renomeando coluna do dataframe produtos
+df_produto = df_produto_normalized.withColumnRenamed("id_produto", "prod_id")
 
 # join estoque e produtos 
-result_df = amazonS3.join(produto_dataset, on='prod_id', how='inner')
-
-# visualizando resultado do join
-result_df.show()
+result_df = df_silver.join(df_produto, on='prod_id', how='inner')
 
 # Selecionando as colunas para o dataframe final
 result_df = result_df.select("prod_id", "qtd", "tipo_de_transacao", "saldo", "data_hora", "nome_produto", "preco_produto", "tipo_produto", "marca_produto")
@@ -67,14 +52,6 @@ result_df_normalized = result_df.withColumn("prod_id", F.col("prod_id").cast(Int
                         .withColumn("tipo_produto", F.col("tipo_produto").cast(StringType())) \
                         .withColumn("marca_produto", F.col("marca_produto").cast(StringType()))
 
-result_df_normalized.show()
-
-# result_df_normalized = result_df_normalized.withColumn("data_hora", F.date_format("data_hora", "dd/MM/yyyy"))
-#                 .withColumn("data_hora", date_format("data_hora", "dd/MM/yyyy HH:mm:ss")) \
-# result_df_normalized.show()
-
 database = "estoque"
 tabela = "produto_estoque_gold"
 result_df_normalized.write.insertInto(f"{database}.{tabela}",overwrite=True)
-
-# job.commit()
